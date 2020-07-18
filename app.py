e4ae81a750df721645e224ee29b83864db9d92bf
Piyush Ghotkar <42354510+Piyush-Ghotkar@users.noreply.github.com>
2020-07-18 15:00:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from scipy.stats import ks_2samp
 from scipy.stats import ttest_ind
 import os
 import json
-import urllib.request as ur_req
+import urllib
 
 
 
@@ -79,9 +79,9 @@ def predict():
              'original duration': org_dur}
     json_data = json.dumps(output).encode()
     
-    request = ur_req.Request("https://healdon-916dd.firebaseio.com/u_id/"+u_id+"/test/voice.json", data=json_data, method="PATCH")
+    request = urllib.request.Request("https://healdon-916dd.firebaseio.com/u_id/"+u_id+"/test/voice.json", data=json_data, method="PATCH")
     try:
-        loader = ur_req.urlopen(request)
+        loader = urllib.request.urlopen(request)
     except urllib.error.URLError as e:
         message = json.loads(e.read())
         
